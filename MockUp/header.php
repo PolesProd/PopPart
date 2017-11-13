fefb2b2c37dda7acfd9503f459566e84644ea5a2
adricen <adrien.centonze@gmail.com>
2017-11-13 09:36:44 +0100

    fichier de mock up premiere page

---
+++ MockUp/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Project Pop Part</title>
+  <link rel="stylesheet" href="assets/lib/bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/lib/startup-ui-kit/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/lib/startup-ui-kit/css/style.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body class="">
+  <section class="cover-2 text-left">
+		<nav class="navbar navbar-toggleable-sm navbar-custom navbar-inverse">
+			<div class="container">
+				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<a class="navbar-brand" href="#">Pop Part.</a>
+				<div class="collapse navbar-collapse pull-xs-right justify-content-end" id="navbarToggler">
+					<ul class="navbar-nav mt-2 mt-md-0 justify-content-end">
+						<li class="nav-item active">
+							<a class="nav-link" href="index.php">Accueil<span class="sr-only">(current)</span></a>
+						</li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">A propos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="team.php">L'équipe</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="territory.php">Les territoires</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="news.php">Actualités</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="scienceCommite.php">Comité scientifique</a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-secondary-yellow pill-btn" href="#">Commons Pops</a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-secondary-yellow pill-btn" href="#">Tryspaces</a>
+            </li>
+					</ul>
+				</div>
+			</div>
+		</nav>
---
+++ MockUp/index.php
@@ -0,0 +1,73 @@
+<?php
+  include('header.php');
+ ?>
+    <div class="img-gradient background">
+      <!-- <img src="https://bootstrapcreative.com/wp-bc/wp-content/uploads/2016/11/pablo.png" width="400" /> -->
+      <div class="bg-img"></div>
+    </div>
+    <section class="content-4">
+			<div class="container">
+				<div class="row justify-center mt-5">
+					<div class="col-md-6 pl-5 pr-5 text-center">
+						<img class="mb-4 img-fluid" src="assets/img/logo_popPart.svg">
+					</div>
+					<div class="col-md-6 text-left">
+						<h2>Le Projet <span class="popPart">Pop. Part</span> qu'est-ce que c'est ?</h2>
+						<p class="lead mt-2 mb-2">Integer libero nunc, elementum eu venenatis sed, molestie non tortor. Duis suscipit pulvinar justo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
+            <p><a class="mb-5 btn btn-outline-white btn-outline" href="#">Plus d'informations</a></p>
+						<div class="row">
+							<div class="col-md-6 col-feature mb-4">
+                <em class="fa fa-2x fa-users"></em>
+								<h4 class="mb-3">L'équipe</h4>
+								<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <p><a class="btn btn-outline-white btn-outline" href="#">Découvrir nos équipes</a></p>
+							</div>
+							<div class="col-md-6 col-feature mb-4">
+                <em class="fa fa-2x fa-globe"></em>
+								<h4 class="mb-3">Le Territoire</h4>
+								<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <p><a class="btn btn-outline-white btn-outline" href="#">Découvrir les territoires</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+    <!-- balise fermant pour le header -->
+	</section>
+  <section class="features-3 text-center">
+			<div class="container mt-5 mb-5">
+				<div class="row justify-center">
+          <div class="col-md-6 text-center overHide">
+            <div id="twitter"></div>
+            <!-- <img class="mt-4 mb-4 img-fluid" src="images/placeholder-beats-x.png"> -->
+          </div>
+					<div class="col-md-5 offset-md-1 text-center text-md-left">
+						<h2>A voir</h2>
+						<p class="lead mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse cursus erat sed sem sagittis cursus.</p>
+            <div class="col-md-9 col-feature mb-5">
+              <em class="fa fa-4x float-left mt-3 mb-3 mr-3 fa-flask "></em>
+              <h4>Comité Scientifique</h4>
+              <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+              <p><a class="btn btn-outline-secondary btn-outline" href="#">Plus d'informations</a></p>
+            </div>
+						<div class="col-md-9 col-feature mb-5">
+							<em class="fa fa-4x float-left mt-3 mb-3 mr-3 fa-linode"></em>
+							<h4>Projets liés</h4>
+							<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+              <p><a class="btn btn-outline-secondary btn-outline" href="#">Plus d'informations</a></p>
+						</div>
+						<div class="col-md-9 col-feature mb-5">
+							<em class="fa fa-4x float-left mt-3 mb-3 mr-3 fa-fire"></em>
+							<h4>Commons Pops</h4>
+							<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+              <p><a class="btn btn-outline-secondary btn-outline" href="#">Plus d'informations</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+  <section class="container"></section>
+  <?php
+    include('footer.php');
+   ?>
